fddff423d176b9cfef0cbda215da3c7888885fc8
Sarou <bastien.hiel@gmail.com>
2019-01-16 21:22:00 +0100

    prettify

--- lib/pap.rb
+++ lib/pap.rb
@@ -4,18 +4,18 @@ require 'twitter'
 
   Dotenv.load
 
-def logtostream
-	client = Twitter::Streaming::Client.new do |config|
+def log_to_stream
+	stream = Twitter::Streaming::Client.new do |config|
 	  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
 	  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
 	  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
 	  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
 	end
-	return client 
+	return stream
 end
 
 
-def login_twitter
+def log_to_twitter
 	client = Twitter::REST::Client.new do |config|
 	  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
 	  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
@@ -24,27 +24,28 @@ def login_twitter
 	end
 	return client 
 end
-	# client = login_twitter
-
-	# topics = ["bonjour_monde"]
-	# client.filter(track: topics.join(",")) do |object|
-	#   puts object.text if object.is_a?(Twitter::Tweet)
-	#   puts tweet.user.screen_name if object.is_a?(Twitter::Tweet)
-	# end
 
 
 
-def stream
- topics = ["#bonjour_monde"]    #saisie de la donnee recherchee
- logtostream.filter(track: topics.join(",")) do |tweet|
- #utilisation du logstreaming pour filtrer les resultat contenant la track: topics
-   puts "#{tweet.user.screen_name}: #{tweet.text}"
-   if tweet.is_a?(Twitter::Tweet)
-        puts "--------------------"
-     login_twitter.favorite(tweet.id)
-     login_twitter.follow(tweet.user)
-   end
- end
+def active_bot
+	client = log_to_twitter
+	stream = log_to_stream
+
+ 	topics = ["#bonjour_monde"]
+
+ 	stream.filter(track: topics.join(",")) do |tweet|
+	 	if tweet.is_a?(Twitter::Tweet)
+		 	puts "-" * 10
+		 	puts "#{tweet.user.screen_name}: #{tweet.text}" 
+		    puts "-" * 5
+		    
+		    client.favorite(tweet.id)
+		    puts "Vous avez aimé le tweet :#{tweet.text}"
+		    puts "-" * 5
+		    client.follow(tweet.user)
+		    puts "vous suivez désormais : #{tweet.user.screen_name}"
+   		end
+ 	end
 end
 
-stream
+active_bot
